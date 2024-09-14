e08c139e5a1334365e46d93f1968c895bdf970ab
Staz6 <98460987+Fozan3060@users.noreply.github.com>
2024-09-15 04:21:13 +0500

    feat: created FlashSalesSection and SectionTitle component

---
+++ src/components/complex/FlashSalesSection.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SectionTitle from "../compound/SectionTitle";
+
+const FlashSalesSection: React.FC = () => {
+  return <SectionTitle title="Today's" />;
+};
+
+export default FlashSalesSection;
