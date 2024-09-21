32c4dc1b3429572c3aa12117c4d4632a05e9b0a2
staz6 <98460987+Fozan3060@users.noreply.github.com>
2024-09-21 03:22:48 -0700

    refactor: improved Section and HomePage component structure

--- src/components/complex/FlashSalesSection.tsx
+++ src/components/complex/FlashSalesSection.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-
-import Section from "./Section";
 import ProductCard from "../compound/ProductCard";
 import productImage from "../../assets/productImage.png";
+import Section from "./Section";
 
 const FlashSalesSection: React.FC = () => {
   return (
-    <Section sectionTitle="Today's" sliderTitle="Flash Sales" showTimer>
-      {Array.from({ length: 6 }).map(() => (
+    <Section
+      sectionTitle="Today's"
+      showPagination
+      sliderTitle="Flash Sales"
+      showTimer
+    >
+      {Array.from({ length: 6 }).map((_, index) => (
         <ProductCard
+          key={index}
           image={productImage}
           title="HAVIT HV-G92 Gamepad"
           price={120}
