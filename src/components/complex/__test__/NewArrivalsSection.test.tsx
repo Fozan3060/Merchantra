f1ef52b428a29819aae6d389c4a5ff481bc03673
Fozan3060 <98460987+Fozan3060@users.noreply.github.com>
2025-04-21 00:09:31 +0500

    chore: updated readme and test cases

--- src/components/complex/__test__/NewArrivalsSection.test.tsx
+++ src/components/complex/__test__/NewArrivalsSection.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import NewArrivalSection from "../NewArrivalSection";
 import "@testing-library/jest-dom";
 
 describe("NewArrivals section tests", () => {
   it("Testing rendering of ui", () => {
-    render(<NewArrivalSection />);
+    render(
+      <MemoryRouter>
+        <NewArrivalSection />
+      </MemoryRouter>,
+    );
     expect(screen.getByText("New Arrival")).toBeInTheDocument();
     expect(screen.getByText("Featured")).toBeInTheDocument();
     expect(screen.getAllByTestId("NewArrivalsProductCard")).toHaveLength(4);
